6a9e1ee4e59604335dab2ae2ff06aa3e679b02e6
Rob Durst <me@robdurst.com>
2024-04-21 20:18:42 -0600

    add all software versions here

--- block_render_engine/api/ide_version.rb
+++ block_render_engine/api/ide_version.rb
@@ -4,6 +4,7 @@ Handler = Proc.new do |request, response|
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
   response.body = {
-    version: Gem.loaded_specs['dtr_core'].version
+    dtr_core_version: Gem.loaded_specs['dtr_core'].version,
+    block_render_engine: '0.1.0'
   }.to_json
 end
