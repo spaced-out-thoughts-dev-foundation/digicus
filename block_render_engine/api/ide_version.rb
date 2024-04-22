afcbf566b5eb8d64c070739399259b861897ea20
Rob Durst <me@robdurst.com>
2024-04-21 20:00:22 -0600

    hardcode version so FE can work for now

--- block_render_engine/api/ide_version.rb
+++ block_render_engine/api/ide_version.rb
@@ -3,5 +3,5 @@ require 'dtr_core'
 Handler = Proc.new do |request, response|
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
-  response.body = Gem.loaded_specs['dtr_core'].version
+  response.body = "0.1.0"
 end
