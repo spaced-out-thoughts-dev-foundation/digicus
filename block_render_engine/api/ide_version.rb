68adeea6e3c4b726a75725b301e2164d56de1b69
Rob Durst <me@robdurst.com>
2024-04-21 20:13:48 -0600

    return json instead of raw text

--- block_render_engine/api/ide_version.rb
+++ block_render_engine/api/ide_version.rb
@@ -3,5 +3,7 @@ require 'dtr_core'
 Handler = Proc.new do |request, response|
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
-  response.body = "0.1.0"
+  response.body = {
+    version: Gem.loaded_specs['dtr_core'].version
+  }.to_json
 end
