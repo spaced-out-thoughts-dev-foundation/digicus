d5fefc9a5e9e5e8a810b0b851533388ee534eb6d
Rob Durst <me@robdurst.com>
2024-05-11 17:50:50 +0200

    enhance compile api to proper accept params

--- block_render_engine/api/compile_from_dtr.rb
+++ block_render_engine/api/compile_from_dtr.rb
@@ -3,7 +3,27 @@ require 'dtr_core'
 Handler = Proc.new do |request, response|
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
+
+  unless request.body 
+    response.body = {
+      received: {
+        dtr_version: Gem.loaded_specs['dtr_core'].version,
+        content: 'Hello, World!'
+      },
+      status: 401
+    }.to_json
+
+    next
+  end
+
+  dtr_version = JSON.parse(request.body)['dtr_version'] || Gem.loaded_specs['dtr_core'].version
+  content = JSON.parse(request.body)['content'] || ''
+
   response.body = {
-    dtr_version: request.body['dtr_version'] || Gem.loaded_specs['dtr_core'].version
+    received: {
+      dtr_version: dtr_version,
+      content: content
+    },
+    status: 200
   }.to_json
 end
