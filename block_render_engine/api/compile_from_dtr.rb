1a4f0a436ae0dd995dfc2fdbc3129c38083a8973
Rob Durst <me@robdurst.com>
2024-05-11 14:19:21 +0200

    use body instead of params

--- block_render_engine/api/compile_from_dtr.rb
+++ block_render_engine/api/compile_from_dtr.rb
@@ -4,6 +4,6 @@ Handler = Proc.new do |request, response|
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
   response.body = {
-    dtr_version: request.params['dtr_version'] || Gem.loaded_specs['dtr_core'].version
+    dtr_version: request.body['dtr_version'] || Gem.loaded_specs['dtr_core'].version
   }.to_json
 end
