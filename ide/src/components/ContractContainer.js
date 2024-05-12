fc3f5ae4dfbf499c3f05207c6a582063693b1830
Rob Durst <me@robdurst.com>
2024-05-12 18:38:01 +0200

    begin trying to use react flow

--- ide/src/components/ContractContainer.js
+++ ide/src/components/ContractContainer.js
@@ -1,51 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Box from '@mui/material/Box';
-import Block from './Block';
+import ReactFlow from 'reactflow';
 
-class ContractContainer extends Component {
-    render() {
-        return (
-          <div style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column',
-              flex: '15',
-              backgroundColor: 'rgb(39 207 230)',
-          }}>
-            <Box
-                style={{
-                  backgroundColor: 'white',
-                  color: 'black',
-                }}
-                height={'100%'}
-                width={'80%'}
-                my={4}
-                display="flex"
-                alignItems="center"
-                gap={4}
-                p={2}
-                sx={{ border: '2px solid grey' }}
-            >
-              {/* <Block /> */}
-              {
-                this.props.functions ?
-                  <div>
-                    <h2>Instructions</h2>
-                    <ol>
-                    {
-                      JSON.parse(this.props.functions)['instructions'].split(' ').map((instructionName) => {
-                        return <li>{instructionName}</li>                    
-                      })
-                    }
-                    </ol>
-                  </div>
-
-                : ''
-              } 
-            </Box>
-          </div>
-        )
-    }
+// TODO: this only works for one function right now
+function ContractContainer({functions}) {
+    return (
+      <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          flex: '15',
+          backgroundColor: 'rgb(39 207 230)',
+      }}>
+        <Box
+            style={{
+              backgroundColor: 'white',
+              color: 'black',
+            }}
+            height={'100%'}
+            width={'80%'}
+            my={4}
+            display="flex"
+            alignItems="center"
+            gap={4}
+            p={2}
+            sx={{ border: '2px solid grey' }}
+        >
+          {
+            functions ?
+              <ReactFlow nodes={JSON.parse(functions[0]).instructions.split(' ').map((instructionName, index) => {
+                return { id: `${index}`, data: { label: instructionName }, position: { x: 100, y: 100 * index } }
+                  })} edges={[]}>
+              {/* <Controls /> */}
+            </ReactFlow> : ''
+          } 
+        </Box>
+      </div>
+    );
 }
 export default ContractContainer;
