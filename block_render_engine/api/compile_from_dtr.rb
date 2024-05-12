91a8d133bc3d9846e979c06e02706447138d79bb
Rob Durst <me@robdurst.com>
2024-05-12 15:05:23 +0200

    extract bre req handler to own class in lib

--- block_render_engine/api/compile_from_dtr.rb
+++ block_render_engine/api/compile_from_dtr.rb
@@ -1,85 +1,8 @@
 require 'dtr_core'
-
-class RequestHandler
-  SUCCESS_STATUS_CODE = 200
-  NO_BODY_STATUS_CODE = 401
-  FAILED_TO_COMPILE_STATUS_CODE = 402
-
-  def initialize(request)
-    @request = request
-  end
-
-  def self.response_body(request)
-    new(request).response_body
-  end
-
-  def response_body
-    return default_response unless @request.body
-
-    compile
-
-    {
-      received: {
-        content: content,
-        format: content_format,
-      },
-      contract_name: @contract_name,
-      contract_state: @contract_state,
-      contract_functions: @contract_functions,
-      compilation_error: @compilation_error,
-      status: status
-    }.to_json
-  end
-
-  private
-
-  def compile
-     begin
-      contract = DTRCore::Contract.from_dtr_raw(content)
-
-      @contract_name = contract.name
-      @contract_state = contract.state
-      @contract_functions = contract.functions.map do |f|
-        { 
-          name: f.name,
-          instructions: f.instructions.map { |x| x[:instruction] }.join(' ')
-        }.to_json
-      end
-
-      @compilation_success = true
-      @compilation_error = ''
-    rescue StandardError => e
-      @contract_name = "Unknown"
-      @contract_state = []
-      @contract_functions = []
-
-      @compilation_error = e
-    end
-  end
-
-  def status
-   @compilation_success ? SUCCESS_STATUS_CODE : FAILED_TO_COMPILE_STATUS_CODE
-  end
-
-  def dtr_core_gem_version
-    Gem.loaded_specs['dtr_core'].version
-  end
-  
-  def content
-    JSON.parse(@request.body)['content'] || ''
-  end
-  
-  def content_format
-    JSON.parse(@request.body)['format'] || 'unknown'
-  end
-
-  def default_response
-    { status: NO_BODY_STATUS_CODE }.to_json
-  end
-end
+require_relative '../lib/block_render_engine_request_handler.rb'
 
 Handler = Proc.new do |request, response|
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
-  response.body = RequestHandler.response_body(request)
+  response.body = BlockRenderEngineRequestHandler.response_body(request)
 end
