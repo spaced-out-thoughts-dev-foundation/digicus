aa37179c827a4fb93227b1c13aa986357b5c5bdf
Rob Durst <me@robdurst.com>
2024-05-12 13:48:34 +0200

    refactor block render engine

--- block_render_engine/api/compile_from_dtr.rb
+++ block_render_engine/api/compile_from_dtr.rb
@@ -1,29 +1,54 @@
 require 'dtr_core'
 
+
+def no_body_status_code
+  401
+end
+
+def success_status_code
+  200
+end
+
+def dtr_core_gem_version
+  Gem.loaded_specs['dtr_core'].version
+end
+
+def default_response
+  {
+    received: {
+      dtr_version: dtr_core_gem_version,
+      content: '',
+      format: 'unknown'
+    },
+    status: no_body_status_code
+  }.to_json
+end
+
+def dtr_version(request)
+  JSON.parse(request.body)['dtr_version'] || dtr_core_gem_version
+end
+
+def content(request)
+  JSON.parse(request.body)['content'] || ''
+end
+
+def content_format(request)
+  JSON.parse(request.body)['format'] || 'unknown'
+end
+
 Handler = Proc.new do |request, response|
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
+  response.body = default_response
 
-  unless request.body 
-    response.body = {
-      received: {
-        dtr_version: Gem.loaded_specs['dtr_core'].version,
-        content: 'Hello, World!'
-      },
-      status: 401
-    }.to_json
-
-    next
-  end
-
-  dtr_version = JSON.parse(request.body)['dtr_version'] || Gem.loaded_specs['dtr_core'].version
-  content = JSON.parse(request.body)['content'] || ''
+  next unless request.body 
 
   response.body = {
     received: {
-      dtr_version: dtr_version,
-      content: content
+      dtr_version: dtr_version(request),
+      content: content(request),
+      format: content_format
     },
-    status: 200
+    status: success_status_code
   }.to_json
 end
