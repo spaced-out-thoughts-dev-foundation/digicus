e2e76777a1e45b6f83c023a5f89a4b02dc2b9102
Rob Durst <me@robdurst.com>
2024-05-14 17:19:56 -0600

    handle multiple function contracts

--- ide/src/components/ContractContainer.js
+++ ide/src/components/ContractContainer.js
@@ -2,6 +2,70 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import ReactFlow, { Controls, MarkerType } from 'reactflow';
 
+function all_nodes(function_data, supportedInstructions, supportedInstructionToColor, function_number) {
+  let function_json_data = JSON.parse(function_data);
+  let all_function_nodes = function_json_data.instructions.split(' ')
+    .filter((instructionName) => !!supportedInstructions.filter(x => x.name.toUpperCase() === instructionName.toUpperCase()))
+    .map((instructionName, index) => {
+      const instructionData = supportedInstructions.filter(x => x.name.toUpperCase() === instructionName.toUpperCase())[0];
+      index += 1;
+      return { id: `${index}|${function_number}`, style: { 
+        backgroundColor:  supportedInstructionToColor(instructionData),
+        textShadow: '1px 1px 1px gray',
+      }, data: { label: instructionData.name.toUpperCase() }, position: { x: 200 * (function_number), y: 100 * (index + 1) } }
+    }) 
+    
+    all_function_nodes.push(
+    { id: `0|${function_number}`, style: { 
+      backgroundColor: 'white',
+      textShadow: '1px 1px 1px gray',
+    }, data: { label: `FUNCTION: [${function_json_data.name}]` }, position: { x: 200 * (function_number), y: 100} })
+    
+    return all_function_nodes;
+}
+
+function all_edges(function_data, function_number) {
+  let function_json_data = JSON.parse(function_data);
+
+  let all_function_edges = function_json_data.instructions.split(' ').slice(1).map((_, index) => {
+    return { 
+      id: `e-${index + 1}|${function_number}`, 
+      source: `${index + 1}|${function_number}`, 
+      target: `${index + 2}|${function_number}`, 
+      markerEnd: {
+        type: MarkerType.ArrowClosed,
+        width: 20,
+        height: 20
+      }}
+  })
+
+  const index = 0;
+
+  all_function_edges.push({
+    id: `e-${index}|${function_number}`, 
+    source: `${index}|${function_number}`, 
+    target: `${index + 1}|${function_number}`, 
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      width: 20,
+      height: 20
+    }});
+
+  return all_function_edges;
+};
+
+function all_function_nodes(functions_data, supportedInstructions, supportedInstructionToColor) {
+  return functions_data.map((function_data, index) => {
+    return all_nodes(function_data, supportedInstructions, supportedInstructionToColor, index);
+  }).flatMap(x => x);
+};
+
+function all_function_edges(functions_data) {
+  return functions_data.map((function_data, index) => {
+    return all_edges(function_data, index);
+  }).flatMap(x => x);
+};
+
 // TODO: this only works for one function right now
 function ContractContainer({functions, supportedInstructions, supportedInstructionToColor}) {
     return (
@@ -29,31 +93,14 @@ function ContractContainer({functions, supportedInstructions, supportedInstructi
         >
           {
             functions ?
-              <ReactFlow nodes={JSON.parse(functions[0]).instructions.split(' ')
-              .filter((instructionName) => !!supportedInstructions.filter(x => x.name.toUpperCase() === instructionName.toUpperCase()))
-              .map((instructionName, index) => {
-                const instructionData = supportedInstructions.filter(x => x.name.toUpperCase() === instructionName.toUpperCase())[0];
-
-                return { id: `${index}`, style: { 
-                  backgroundColor:  supportedInstructionToColor(instructionData),
-                  textShadow: '1px 1px 1px gray',
-                }, data: { label: instructionData.name.toUpperCase() }, position: { x: 100, y: 100 * index } }
-                  })} edges={JSON.parse(functions[0]).instructions.split(' ').slice(1).map((instructionName, index) => {
-                    return { 
-                      id: `e-${index}`, 
-                      source: `${index}`, 
-                      target: `${index + 1}`, 
-                      markerEnd: {
-                        type: MarkerType.ArrowClosed,
-                        width: 20,
-                        height: 20
-                      }}
-                })}
-                fitView={true}
-                panOnDrag={false}
-                zoomOnScroll={false}
-                zoomOnPinch={false}
-                zoomOnDoubleClick={false}
+              <ReactFlow 
+                nodes={all_function_nodes(functions, supportedInstructions, supportedInstructionToColor)} 
+                edges={all_function_edges(functions)}
+                  fitView={true}
+                  panOnDrag={false}
+                  zoomOnScroll={false}
+                  zoomOnPinch={false}
+                  zoomOnDoubleClick={false}
                 >
             </ReactFlow> : ''
           } 
