6a37feba7d150958cfdb43a0d9dbc1704525b27b
Rob Durst <me@robdurst.com>
2024-05-14 07:23:25 -0600

    fix casing typo

--- block_render_engine/api/supported_types_and_instructions.rb
+++ block_render_engine/api/supported_types_and_instructions.rb
@@ -4,7 +4,7 @@ Handler = Proc.new do |request, response|
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
   response.body = {
-    supported_instructions: DTRCore::SupportedAttributes::Instructions,
-    supported_types: DTRCore::SupportedAttributes::Types
+    supported_instructions: DTRCore::SupportedAttributes::INSTRUCTIONS,
+    supported_types: DTRCore::SupportedAttributes::TYPES
   }.to_json
 end
