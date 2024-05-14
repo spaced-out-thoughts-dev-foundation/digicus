872d384c0a44a805f4ea448341f36b82e8441aa2
Rob Durst <me@robdurst.com>
2024-05-14 07:19:03 -0600

    fix path issue

--- block_render_engine/api/supported_types_and_instructions.rb
+++ block_render_engine/api/supported_types_and_instructions.rb
@@ -4,7 +4,7 @@ Handler = Proc.new do |request, response|
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
   response.body = {
-    supported_instructions: DTRCore::Instructions,
-    supported_types: DTRCore::Types
+    supported_instructions: DTRCore::SupportedAttributes::Instructions,
+    supported_types: DTRCore::SupportedAttributes::Types
   }.to_json
 end
