800db4b9f3458f90469a13d885870e60977419dd
Rob Durst <me@robdurst.com>
2024-05-19 12:15:19 -0600

    finally figured out local dev. fixed this

--- temp_rust_to_dtr_serve/api/handler.rs
+++ temp_rust_to_dtr_serve/api/handler.rs
@@ -1,27 +1,50 @@
-// extern crate rust_to_dtr;
-
 use serde_json::json;
+use serde::Deserialize;
 use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
-// use rust_to_dtr;
+use rust_to_dtr::version;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     run(handler).await
 }
 
+#[derive(Deserialize)]
+struct RequestBody {
+    content: String,
+}
+
 pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
-    let body = req.body().await?;
-    let body = String::from_utf8(body.to_vec())?;
+    let whole_body = req.body();
+    let body = String::from_utf8(whole_body.to_vec())?;
     println!("Request body: {}", body);
+
+    let parsed: RequestBody = match serde_json::from_str(&body) {
+        Ok(parsed) => parsed,
+        Err(e) => {
+            return Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .header("Content-Type", "application/json")
+                .body(
+                    json!({
+                        "message": "Invalid request body",
+                        "error": e.to_string(),
+                    })
+                    .to_string()
+                    .into(),
+                )?)
+        }
+    };
 
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
         .body(
             json!({
-              "message": "你好，世界",
-              "body": body
-            //   "rust_to_dtr_version": rust_to_dtr::version(),
+                "message": "你好，世界",
+                "body_raw_string": body,
+                "content": parsed.content,
+                "rust_to_dtr_version": rust_to_dtr::version(),
+                "temp_rust_to_dtr_version": env!("CARGO_PKG_VERSION")
             })
             .to_string()
             .into(),
