643207533404ae1912e27a9eee4578e7b6787fa8
Rob Durst <me@robdurst.com>
2024-05-19 01:05:59 -0600

    port 8080

--- block_render_engine/puma.rb
+++ block_render_engine/puma.rb
@@ -2,6 +2,6 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['THREAD_COUNT'] || 5)
 threads threads_count, threads_count
 
-port        ENV['PORT']     || 8888
+port        ENV['PORT']     || 8080
 environment ENV['RACK_ENV'] || 'development'
 # tcp_host "0.0.0.0"
