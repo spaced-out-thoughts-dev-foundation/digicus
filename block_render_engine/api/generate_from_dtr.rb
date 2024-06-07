b67d4c0eddac2303e5ee3817c56614648dbb2089
Rob Durst <me@robdurst.com>
2024-06-06 23:35:57 -0600

    begin generate_from_dtr endpoint in BRE

--- block_render_engine/api/generate_from_dtr.rb
+++ block_render_engine/api/generate_from_dtr.rb
@@ -1,12 +1,10 @@
-require 'dtr_core'
 require 'dtr_to_rust'
+
 
 Handler = Proc.new do |request, response|
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
   response.body = {
-    dtr_core: Gem.loaded_specs['dtr_core'].version,
-    dtr_to_rust: Gem.loaded_specs['dtr_to_rust'].version,
-    block_render_engine: '0.5.0'
+   "hello": "world"
   }.to_json
 end
