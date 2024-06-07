0729ffd0cd581370005be5a0f05e0fc530f3ab4a
Rob Durst <me@robdurst.com>
2024-06-06 23:42:47 -0600

    test run return rust from dtr hardcoded

--- block_render_engine/api/generate_from_dtr.rb
+++ block_render_engine/api/generate_from_dtr.rb
@@ -1,10 +1,28 @@
 require 'dtr_to_rust'
 
-
 Handler = Proc.new do |request, response|
+  dtr_code = <<~DTR
+[Contract]: AnswerToLifeContract
+
+[InternalFunctions]:
+  -() [fourty_two]
+  * Inputs:
+  {
+    env: Env
+  }
+  * Output: u32
+  * Instructions:
+    $
+      { instruction: Return, input: (42), scope: 0 }
+    $
+  :[InternalFunctions]
+  [User Defined Types]:
+  :[User Defined Types]
+  DTR
+
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
   response.body = {
-   "hello": "world"
+   "body": DTRToRust::Generator.generate_from_string(dtr_code)
   }.to_json
 end
