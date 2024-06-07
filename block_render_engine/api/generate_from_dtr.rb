6a19866812dc1530ef54e578f39914c2075d5ea3
Rob Durst <me@robdurst.com>
2024-06-07 10:27:09 -0600

    add debugging logs bre

--- block_render_engine/api/generate_from_dtr.rb
+++ block_render_engine/api/generate_from_dtr.rb
@@ -8,6 +8,9 @@ require 'json'
 
 Handler = Proc.new do |request, response|
   begin
+
+    puts "Request body: #{request.body}"
+
     contract = DTRCore::Contract.new(
       request.body.contract.contract_name,
       request.body.contract.contract_state,
@@ -15,8 +18,15 @@ Handler = Proc.new do |request, response|
       request.body.contract.contract_user_defined_types
     )
 
+    puts "[DEBUG]: formed contract"
+
     dtr_code = contract.to_s
+
+    puts "[DEBUG]: generated DTR code: #{dtr_code}"
+
     rust_code = DTRToRust::DtrToRust::Generator.generate_from_string(dtr_code)
+
+    puts "[DEBUG]: generated Rust code: #{rust_code}"
 
     response.status = 200
     response['Content-Type'] = 'text/text; charset=utf-8'
