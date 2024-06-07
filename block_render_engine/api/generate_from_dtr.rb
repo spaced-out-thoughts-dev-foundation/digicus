7bb917b33e09ed435f50cbfd8ebcf856093f2499
Rob Durst <me@robdurst.com>
2024-06-07 11:44:19 -0600

    add debugging logs bre

--- block_render_engine/api/generate_from_dtr.rb
+++ block_render_engine/api/generate_from_dtr.rb
@@ -10,7 +10,7 @@ Handler = Proc.new do |request, response|
   begin
     puts "Received request to generate Rust code from DTR code"
     puts "[Debug]: contract_name: #{request.body['contract_name']}"
-    puts "[Debug]: contract_state: #{request.body.['contract_state']}"
+    puts "[Debug]: contract_state: #{request.body['contract_state']}"
     puts "[Debug]: contract_functions: #{request.body['contract_functions']}"
     puts "[Debug]: contract_user_defined_types: #{request.body['contract_user_defined_types']}"
 
