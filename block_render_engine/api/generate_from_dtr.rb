e75d1c1cf7398908de07ac19891fa28f1dfd3d16
Rob Durst <me@robdurst.com>
2024-06-07 01:19:09 -0600

    bre gen endpoint takes arg

--- block_render_engine/api/generate_from_dtr.rb
+++ block_render_engine/api/generate_from_dtr.rb
@@ -1,28 +1,9 @@
 require 'dtr_to_rust'
 
 Handler = Proc.new do |request, response|
-  dtr_code = <<~DTR
-[Contract]: AnswerToLifeContract
-
-[InternalFunctions]:
-  -() [fourty_two]
-  * Inputs:
-  {
-    env: Env
-  }
-  * Output: u32
-  * Instructions:
-    $
-      { instruction: Return, input: (42), scope: 0 }
-    $
-  :[InternalFunctions]
-  [User Defined Types]:
-  :[User Defined Types]
-  DTR
-
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
   response.body = {
-   "body": DTRToRust::Generator.generate_from_string(dtr_code)
+   "body": request.body
   }.to_json
 end
