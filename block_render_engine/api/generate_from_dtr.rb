6800495665f6740d3a03b2531d812e1de3b26a2b
Rob Durst <me@robdurst.com>
2024-06-07 12:08:49 -0600

    bump 0.5.5 for dtr_to_core for BRE

--- block_render_engine/api/generate_from_dtr.rb
+++ block_render_engine/api/generate_from_dtr.rb
@@ -9,18 +9,16 @@ require 'json'
 Handler = Proc.new do |request, response|
   begin
     puts "Received request to generate Rust code from DTR code: #{request.body}"
-    puts "[Debug]: contract_name: #{request.body['contract_name']}"
-    puts "[Debug]: contract_state: #{request.body['contract_state']}"
-    puts "[Debug]: contract_functions: #{request.body['contract_functions']}"
+    puts "[Debug]: contract_name: #{JSON.parse(request.body)['contract_name']}"
+    puts "[Debug]: contract_state: #{JSON.parse(request.body)['contract_state']}"
     puts "[Debug]: contract_functions: #{JSON.parse(request.body)['contract_functions']}"
-
-    puts "[Debug]: contract_user_defined_types: #{request.body['contract_user_defined_types']}"
+    puts "[Debug]: contract_user_defined_types: #{JSON.parse(request.body)['contract_user_defined_types']}"
 
     contract = DTRCore::Contract.new(
-      request.body['contract_name'],
-      request.body['contract_state'],
-      request.body['contract_functions'],
-      request.body['contract_user_defined_types']
+      JSON.parse(request.body)['contract_name'],
+      JSON.parse(request.body)['contract_state'],
+      JSON.parse(request.body)['contract_functions'],
+      JSON.parse(request.body)['contract_user_defined_types']
     )
 
     puts "[DEBUG]: formed contract"
