45b2d0fd8f63e19fe55cb8a7e70c614928939872
Rob Durst <me@robdurst.com>
2024-06-07 10:23:16 -0600

    dtr elements --> dtr --> rust in BRE

--- block_render_engine/api/generate_from_dtr.rb
+++ block_render_engine/api/generate_from_dtr.rb
@@ -1,9 +1,34 @@
 require 'dtr_to_rust'
+require 'dtr_core'
+require 'fiddle'
+require 'fiddle/import'
+require 'net/http'
+require 'uri'
+require 'json'
 
 Handler = Proc.new do |request, response|
-  response.status = 200
-  response['Content-Type'] = 'text/text; charset=utf-8'
-  response.body = {
-   "body": request.body
-  }.to_json
+  begin
+    contract = DTRCore::Contract.new(
+      request.body.contract.contract_name,
+      request.body.contract.contract_state,
+      request.body.contract.contract_functions,
+      request.body.contract.contract_user_defined_types
+    )
+
+    dtr_code = contract.to_s
+    rust_code = DTRToRust::DtrToRust::Generator.generate_from_string(dtr_code)
+
+    response.status = 200
+    response['Content-Type'] = 'text/text; charset=utf-8'
+    response.body = {
+      "dtr_code": dtr_code,
+      "rust_code": rust_code
+    }.to_json
+  rescue => e
+    response.status = 500
+    response['Content-Type'] = 'text/text; charset=utf-8'
+    response.body = {
+      "error": e.message
+    }.to_json
+  end
 end
