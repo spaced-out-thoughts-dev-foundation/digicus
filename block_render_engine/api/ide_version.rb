5cbf02ed53b93c130c3244284c64686845a35a88
Rob Durst <me@robdurst.com>
2024-06-19 21:11:04 -0600

    updating all deps, need to automate this...

--- block_render_engine/api/ide_version.rb
+++ block_render_engine/api/ide_version.rb
@@ -1,12 +1,12 @@
 require 'dtr_core'
 require 'dtr_to_rust'
 
-Handler = Proc.new do |request, response|
+Handler = proc do |_request, response|
   response.status = 200
   response['Content-Type'] = 'text/text; charset=utf-8'
   response.body = {
     dtr_core: Gem.loaded_specs['dtr_core'].version,
     dtr_to_rust: Gem.loaded_specs['dtr_to_rust'].version,
-    block_render_engine: '0.6.4'
+    block_render_engine: '0.7.0'
   }.to_json
 end
