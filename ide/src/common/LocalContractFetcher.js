3eea87221e64438e787ede2d6a7a380a275c3c36
Rob Durst <me@robdurst.com>
2024-07-14 12:13:45 -0600

    fix contract

--- ide/src/common/LocalContractFetcher.js
+++ ide/src/common/LocalContractFetcher.js
@@ -1,15 +1,15 @@
 const contractDictionary = {
     "hello_world_logging": `#![no_std]
-use soroban_sdk::{contract, contractimpl, log, Env, String};
+use soroban_sdk::{contract, contractimpl, log, Env, Symbol};
 
 #[contract]
 pub struct Contract;
 
 #[contractimpl]
 impl Contract {
-    pub fn hello(env: Env, value: String) {
+    pub fn hello(env: Env, answer_to_life: number, value: Symbol) {
         if value == "fizz" {
-            panic!(&env, "Buzz!");
+            panic!("Not the answer to life!");
         }
 
         log!(&env, "Hello {}", value);
