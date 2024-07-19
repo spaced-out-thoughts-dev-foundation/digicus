b5e09c897b754e418bce2066460517e32fdaacaa
Rob Durst <me@robdurst.com>
2024-07-18 23:15:07 -0600

    add dd rum

--- ide/src/App.js
+++ ide/src/App.js
@@ -11,6 +11,42 @@ import TopHeaderBar from './components/TopHeaderBar';
 import InstructionsAndActionsSideBar from './components/InstructionsAndActionsSideBar';
 import { saveAs } from 'file-saver';
 import { localContractFetch } from './common/LocalContractFetcher';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Typography from '@mui/material/Typography';
+import packageJson from '../package.json';
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+  applicationId: 'b2fafb9d-88bd-4a39-8a65-9c5a09238b31',
+  clientToken: 'pubf338b26f195838c202f865b849c1bb76',
+  // `site` refers to the Datadog site parameter of your organization
+  // see https://docs.datadoghq.com/getting_started/site/
+  site: 'datadoghq.com',
+  service: 'digicus',
+  env: 'prod',
+  // Specify a version number to identify the deployed version of your application in Datadog
+  version: packageJson.version,
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 10,
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: 'mask-user-input',
+});
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
 
 const App = () => {
   const [contract, setContract] = useState({ contract: '', originalText: ``, generatedText: `` });
@@ -18,6 +54,14 @@ const App = () => {
   const [supportedInstructions, setSupportedInstructions] = useState([]);
   const [showCodeContainer, setShowCodeContainer] = useState(true);
   const [showUserDefinedTypes, setShowUserDefinedTypes] = useState(false);
+  const [open, setOpen] = React.useState(true);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const BASE_URL = "https://api.digicus.dev";
 
@@ -212,6 +256,48 @@ const App = () => {
 
   return (
     <div className='top-level-div-container'>
+
+      {
+        open ? <BootstrapDialog
+          onClose={handleClose}
+          aria- labelledby="customized-dialog-title"
+          open={open}
+        >
+          <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+            <center> <h1>Welcome to Digicus</h1><i><strong>[The Demo Version]</strong>: {packageJson.version}</i></center>
+          </DialogTitle>
+          <DialogContent dividers>
+
+            <Typography gutterBottom>
+              Understanding smart contracts is <strong>hard</strong>. So, we created Digicus, <i>scratch for smart contracts. </i>
+              We believe exploring smart contracts in our visual, block-based drag and drop editor will empower the next generation of Soroban
+              developers.
+            </Typography>
+            <Typography gutterBottom>
+              <br></br>
+              <strong>How to use:</strong>
+              <ol>
+                <li>Upload a contract file in the top right. In <i>demo mode</i> contracts are limited to a subset of SDF's official examples.</li>
+                <li>Once uploaded, explore the contract by dragging and pinching within the view window. It is in <i>edit mode</i> by default. To exit edit mode, click the small lock icon in the bottom right of the viewer.</li>
+                <li>On the right side below the upload, switch between the original code and <i>Digit's</i> generated code. At this time the generated code is <i>not as clean as the original</i>, yet it is functionally the same.</li>
+                <li>Within the contract viewer, input boxes are editable. Watch your changes mirrored in the generated code (in <i>almost</i> real time).</li>
+                <li>When ready, click <i>Save</i> to download the generated code.</li>
+              </ol>
+            </Typography>
+            <br></br>
+            <div style={{ fontFamily: "'Playwrite IT Moderna', cursive" }}>
+              <strong><a href="https://spaced-out-thoughts-dev-foundation.github.io/">We are the Spaced Out Thoughts Development Foundation</a></strong>,
+              a small Ruby shop determined to make programming smart contracts as easy as possible.
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleClose}>
+              Begin
+            </Button>
+          </DialogActions>
+        </BootstrapDialog > : null
+      }
+
       <AppBar position="static" className='top-app-bar'>
         <TopHeaderBar />
         <div className='top-level-second-level-container'>
@@ -271,9 +357,10 @@ const App = () => {
           </div>
         </div>
       </AppBar>
+
       <SpeedInsights />
       <Analytics />
-    </div>
+    </div >
   )
 }
 export default App;
