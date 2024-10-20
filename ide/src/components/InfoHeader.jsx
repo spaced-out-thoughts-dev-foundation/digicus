70f55f7edd5c8b8d77f1dcd51c204382b0766b56
Rob Durst <me@robdurst.com>
2024-10-19 18:24:00 -0600

    one last css update for service status button

--- ide/src/components/InfoHeader.jsx
+++ ide/src/components/InfoHeader.jsx
@@ -5,8 +5,8 @@ import '.././styles/InfoHeader.css';
 function InfoHeader() {
   return (
     <div className='info-header' style={{ textAlign: 'right' }}>
-      <button style={{ width: '50%', backgroundColor: '#27cfe6', border: 'none', padding: '10px', color: 'white', cursor: 'pointer', borderRadius: '5px', marginBottom: '10px' }}>
-        <a href='https://spaced-out-thoughts-development-foundation.instatus.com/' target='_blank' rel='noopener noreferrer' style={{ textDecoration: 'none', color: 'inherit', fontSize: '0.75vw' }}>
+      <button style={{ width: '50%', backgroundColor: '#26bee6', border: 'none', padding: '10px', color: 'white', cursor: 'pointer', borderRadius: '5px', marginBottom: '10px' }}>
+        <a href='https://spaced-out-thoughts-development-foundation.instatus.com/' target='_blank' rel='noopener noreferrer' style={{ textDecoration: 'none', color: 'inherit', fontSize: '0.85vw' }}>
           Service Status
         </a>
       </button>
