0bdcebd309ca29334f5b58bc5d20c1e22d4d7481
Rob Durst <me@robdurst.com>
2024-10-19 18:22:34 -0600

    simple css for service status button update

--- ide/src/components/InfoHeader.jsx
+++ ide/src/components/InfoHeader.jsx
@@ -5,7 +5,7 @@ import '.././styles/InfoHeader.css';
 function InfoHeader() {
   return (
     <div className='info-header' style={{ textAlign: 'right' }}>
-      <button style={{ width: '20%', backgroundColor: '#27cfe6', border: 'none', padding: '10px', color: 'white', cursor: 'pointer', borderRadius: '5px', marginBottom: '10px' }}>
+      <button style={{ width: '50%', backgroundColor: '#27cfe6', border: 'none', padding: '10px', color: 'white', cursor: 'pointer', borderRadius: '5px', marginBottom: '10px' }}>
         <a href='https://spaced-out-thoughts-development-foundation.instatus.com/' target='_blank' rel='noopener noreferrer' style={{ textDecoration: 'none', color: 'inherit', fontSize: '0.75vw' }}>
           Service Status
         </a>
